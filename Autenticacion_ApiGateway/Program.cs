011ab9b78025bdc2c1a8c74775ab1971c3135140
Abigail Acuña <acuaabigail@yahoo.com>
2024-08-30 13:31:43 -0600

    Subiendo desarrollo de Habilidades

--- Autenticacion_ApiGateway/Program.cs
+++ Autenticacion_ApiGateway/Program.cs
@@ -1,15 +1,64 @@
-namespace Autenticacion_ApiGateway
+﻿using LibrosApiAutenticacion.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.Threading.Tasks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
+
+builder.Services.AddDbContext<LibroContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+var app = builder.Build();
+
+// autenticación para API Key
+app.Use(async (context, next) =>
 {
-    public class Program
+    const string apiKeyHeaderName = "Authorization";
+    const string apiKey = "Api123456"; 
+
+    if (context.Request.Headers.TryGetValue(apiKeyHeaderName, out var extractedApiKey))
     {
-        public static void Main(string[] args)
+        if (extractedApiKey == apiKey)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World!");
-
-            app.Run();
+            // La autenticación fue exitosa
+            context.Response.Headers.Add("X-ApiKey-Status", "Valid");
+            await next();
+            return;
+        }
+        else
+        {
+            // La clave API es incorrecta
+            context.Response.ContentType = "text/plain";
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await context.Response.WriteAsync("No autenticado, La clave API es incorrecta, verifique sus datos");
+            return;
         }
     }
-}
+
+    // No se proporcionó ninguna clave API
+    context.Response.ContentType = "text/plain";
+    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+    await context.Response.WriteAsync("No autenticado, La clave API no fue proporcionada, intenta nuevamente");
+});
+
+app.UseRouting();
+app.UseAuthorization();
+
+
+await app.UseOcelot();
+
+
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation("Ocelot is running on port {Port}", builder.Configuration["GlobalConfiguration:BaseUrl"]?.Split(':').Last());
+
+app.Run();
