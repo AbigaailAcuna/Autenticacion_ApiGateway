8b6264b35030a91cad5ba7dc44b0003f11e8b845
Abigail Acuña <acuaabigail@yahoo.com>
2024-08-30 12:24:17 -0600

    Agregar archivos de proyecto.

---
+++ LibrosApiAutenticacion/Models/Libro.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibrosApiAutenticacion.Models
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Titulo { get; set; } = string.Empty;
+        public string Autor { get; set; }
+        public int AnioPublicacion { get; set; }
+    }
+}
---
+++ LibrosApiAutenticacion/Models/LibroContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibrosApiAutenticacion.Models
+{
+    public class LibroContext : DbContext
+    {
+        public LibroContext(DbContextOptions<LibroContext> options)
+           : base(options) { }
+        public DbSet<Libro> Libros { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Libro>().HasData(
+                new Libro
+                {
+                    Id = 1,
+                    Titulo = "Cien años de soledad",
+                    Autor = "Gabriel García Márquez",
+                    AnioPublicacion = 1967
+                },
+                new Libro
+                {
+                    Id = 2,
+                    Titulo = "Hija de la Tierra",
+                    Autor = "Andrea Longarela",
+                    AnioPublicacion = 2024
+                },
+                new Libro
+                {
+                    Id = 3,
+                    Titulo = "El amor en tiempos del cólera",
+                    Autor = "Gabriel García Márquez",
+                    AnioPublicacion = 1985
+                }
+                );
+        }
+    }
+}
